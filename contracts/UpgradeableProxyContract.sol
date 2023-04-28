c95ee0bf1d1fa8a7d0c2a15e7b301ca76eda4c96
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-28 16:04:13 +0800

    declaring manager and user roles

--- contracts/UpgradeableProxyContract.sol
+++ contracts/UpgradeableProxyContract.sol
@@ -6,4 +6,6 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract UpgradeableProxyContract is ERC20Upgradeable, AccessControlUpgradeable {
+    bytes32 public constant MANAGER = keccak256("MANAGER");
+    bytes32 public constant USER = keccak256("USER");
 }
