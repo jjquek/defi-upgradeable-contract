39c1dfbff6989a774e082351b5e762874b56d080
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-29 16:09:29 +0800

    added and used solidity prettier plugin

--- contracts/UpgradeableProxyContract.sol
+++ contracts/UpgradeableProxyContract.sol
@@ -12,16 +12,21 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeab
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract UpgradeableProxyContract is Initializable, ERC20Upgradeable, AccessControlUpgradeable, ReentrancyGuardUpgradeable  {
+contract UpgradeableProxyContract is
+    Initializable,
+    ERC20Upgradeable,
+    AccessControlUpgradeable,
+    ReentrancyGuardUpgradeable
+{
     // need the interface for ERC20Upgradeable to interact with API provided by the SafeERC20Upgradeable contract.
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
     // * --------- CUSTOM EVENTS -----------
     // for transprancy and consistency's sake, we'll emit events for any of the transactions that occur for Users. We make our own events wherever our Base Contracts do not already provide suitable events.
-    event EtherDeposited (address depositor, uint256 amount);
-    event TKNDeposited (address depositor, uint256 amount);
-    event EtherWithdrawn (address withdrawer, uint256 amount);
-    event TKNWithdrawn (address withdrawer, uint256 amount);
+    event EtherDeposited(address depositor, uint256 amount);
+    event TKNDeposited(address depositor, uint256 amount);
+    event EtherWithdrawn(address withdrawer, uint256 amount);
+    event TKNWithdrawn(address withdrawer, uint256 amount);
     // * --------- STATE VARIABLES -----------
     // ---- roles ---
     bytes32 public constant MANAGER = keccak256("MANAGER");
@@ -32,20 +37,23 @@ contract UpgradeableProxyContract is Initializable, ERC20Upgradeable, AccessCont
     uint256[50] private __gap; // extra storage space for future upgrade variables- 50 being roughly the space needed for another mapping like _etherBalances for 100 users.
 
     // * --------- PUBLIC / EXTERNAL FUNCTIONS -----------
-    function initialize(address _manager) public initializer {
+    function initialize(address manager) public initializer {
         // we initialise the base contracts as per inheritance requirements, and modify this function with initializer to ensure it is called only once-- like a constructor
         __ERC20_init("My Token", "TKN"); // name and symbol is up to us.
         __AccessControl_init();
         // set up various roles
-        _setupRole(DEFAULT_ADMIN_ROLE, _manager);
-        _setupRole(MANAGER, _manager);
+        _setupRole(DEFAULT_ADMIN_ROLE, manager);
+        _setupRole(MANAGER, manager);
         _setRoleAdmin(USER, MANAGER);
     }
 
     // * --------- deposit -----------
     function depositEther() external payable {
         uint256 amount = msg.value;
-        require(amount > 0, "depositEther: Deposit amount must be greater than 0");
+        require(
+            amount > 0,
+            "depositEther: Deposit amount must be greater than 0"
+        );
         if (!hasRole(USER, msg.sender)) {
             grantRole(USER, msg.sender);
         }
@@ -54,20 +62,35 @@ contract UpgradeableProxyContract is Initializable, ERC20Upgradeable, AccessCont
     }
 
     function depositTKN(address tokenContractAddress, uint256 amount) external {
-        require(amount > 0, "depositTKN: Deposit amount must be greater than 0");
+        require(
+            amount > 0,
+            "depositTKN: Deposit amount must be greater than 0"
+        );
         if (!hasRole(USER, msg.sender)) {
             grantRole(USER, msg.sender);
         }
-        SafeERC20Upgradeable.safeTransferFrom(IERC20Upgradeable(tokenContractAddress), msg.sender, address(this), amount);
+        SafeERC20Upgradeable.safeTransferFrom(
+            IERC20Upgradeable(tokenContractAddress),
+            msg.sender,
+            address(this),
+            amount
+        );
         _mint(msg.sender, amount);
         emit TKNDeposited(msg.sender, amount);
     }
 
-
     // * --------- withdraw -----------
-    function withdrawEther(uint256 amount) external onlyRole(USER) nonReentrant {
-        require(amount > 0, "withdrawEther: Withdrawal amount must be greater than 0");
-        require(_etherBalances[msg.sender] >= amount, "withdrawEther: Insufficient balance");
+    function withdrawEther(
+        uint256 amount
+    ) external onlyRole(USER) nonReentrant {
+        require(
+            amount > 0,
+            "withdrawEther: Withdrawal amount must be greater than 0"
+        );
+        require(
+            _etherBalances[msg.sender] >= amount,
+            "withdrawEther: Insufficient balance"
+        );
 
         // Update balances
         _etherBalances[msg.sender] -= amount;
@@ -77,14 +100,27 @@ contract UpgradeableProxyContract is Initializable, ERC20Upgradeable, AccessCont
         emit EtherWithdrawn(msg.sender, amount);
     }
 
-    function withdrawTKN(address tokenContractAddress, uint256 amount) external onlyRole(USER) nonReentrant {
-        require(amount > 0, "withdrawTKN: Withdrawal amount must be greater than 0");
-        require(balanceOf(msg.sender) >= amount, "withdrawTKN: Insufficient balance");
+    function withdrawTKN(
+        address tokenContractAddress,
+        uint256 amount
+    ) external onlyRole(USER) nonReentrant {
+        require(
+            amount > 0,
+            "withdrawTKN: Withdrawal amount must be greater than 0"
+        );
+        require(
+            balanceOf(msg.sender) >= amount,
+            "withdrawTKN: Insufficient balance"
+        );
 
         // Update balances
         _burn(msg.sender, amount);
         // Transfer tokens to the user- Note: safeTransfer throws error if transfer doesn't succeed.
-        SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(tokenContractAddress), msg.sender, amount); // reentrancy vulnerablity is less here compared to in withdrawEther, because fallback functions of the attack contract are not triggered by the safeTransfer call. However, using a transfer function from an external contract we do not control means there is still the possibility of an attack. Note: need to weigh gas costs vs. vulnerability concerns.
+        SafeERC20Upgradeable.safeTransfer(
+            IERC20Upgradeable(tokenContractAddress),
+            msg.sender,
+            amount
+        ); // reentrancy vulnerablity is less here compared to in withdrawEther, because fallback functions of an attack contract are not triggered by the safeTransfer call. However, using a transfer function from an external contract we do not control means there is still the possibility of an attack. Note: need to weigh gas costs vs. vulnerability concerns.
         emit TKNWithdrawn(msg.sender, amount);
     }
 
