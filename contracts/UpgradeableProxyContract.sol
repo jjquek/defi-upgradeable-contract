286ff8b7c640c9b2657ad15388aeb6c9d67df3de
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-29 15:04:34 +0800

    added openzepp dependencies

--- contracts/UpgradeableProxyContract.sol
+++ contracts/UpgradeableProxyContract.sol
@@ -2,8 +2,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// * --------- OPENZEPELLIN PACKAGES -----------
+// A straightforward and robust way to implement upgradeable contracts is to extend contracts provided by OpenZepellin. Their contracts have been the subject of much effort and audit.
+// See inline comments where these imports are used for specific reasons.
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
 contract UpgradeableProxyContract is ERC20Upgradeable, AccessControlUpgradeable {
     bytes32 public constant MANAGER = keccak256("MANAGER");
