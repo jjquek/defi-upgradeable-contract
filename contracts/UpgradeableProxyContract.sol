429bc4ce5a3fee160928f1d8b93bc3a10408eec2
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-29 15:11:42 +0800

    Custom events and inheritance defined

--- contracts/UpgradeableProxyContract.sol
+++ contracts/UpgradeableProxyContract.sol
@@ -12,7 +12,18 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeab
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract UpgradeableProxyContract is ERC20Upgradeable, AccessControlUpgradeable {
+contract UpgradeableProxyContract is Initializable, ERC20Upgradeable, AccessControlUpgradeable, ReentrancyGuardUpgradeable  {
+    // need the interface for ERC20Upgradeable to interact with API provided by the SafeERC20Upgradeable contract.
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // * --------- CUSTOM EVENTS -----------
+    // for transprancy and consistency's sake, we'll emit events for any of the transactions that occur for Users. We make our own events wherever our Base Contracts do not already provide suitable events.
+    event EtherDeposited (address depositor, uint256 amount);
+    event TKNDeposited (address depositor, uint256 amount);
+    event EtherWithdrawn (address withdrawer, uint256 amount);
+    event TKNWithdrawn (address withdrawer, uint256 amount);
+    // * --------- STATE VARIABLES -----------
+    // ---- roles ---
     bytes32 public constant MANAGER = keccak256("MANAGER");
     bytes32 public constant USER = keccak256("USER");
 }
