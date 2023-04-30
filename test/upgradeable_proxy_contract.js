6fca3872d94b23be5b6126cf4459edce60aa60fb
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-04-30 19:52:31 +0800

    removing stale comments

--- test/upgradeable_proxy_contract.js
+++ test/upgradeable_proxy_contract.js
@@ -11,11 +11,6 @@ contract("UpgradeableProxyContract", function (accounts) {
   beforeEach(async () => {
     this.contract = await deployProxy(UpgradeableProxyContract);
   });
-  // todo : I want to check whether the address that deploys the contract is set up as the manager.
-  /*
-   * I think I know the address that deploys it via truffle develop && migrate. Want to verify the difference between Contract Address and Account.
-   * I need to check, after that, who provides the manager
-   */
   it("should grant the manager who deploys the contract the MANAGER role", async () => {
     const managerAddress = accounts[0]; // 'deploy' or 'migrate' in truffle develop uses the first address as the account address that deploys the contract.
     const isManager = await this.contract.hasRole(
