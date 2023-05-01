ed1c9f7306fd00006d0a880ba85a107b166de61d
Barry Quek JJ <barry.quek@generalassemb.ly>
2023-05-01 10:26:51 +0800

    using BDD-style chai API and editing test code for greater clarity

--- migrations/1_basic_deploy.js
+++ migrations/1_basic_deploy.js
@@ -3,8 +3,8 @@ const UpgradeableProxyContract = artifacts.require("UpgradeableProxyContract");
 
 module.exports = async function (deployer) {
   // deployment steps
-  const instance = await deployProxy(UpgradeableProxyContract, [], {
+  const proxyInstance = await deployProxy(UpgradeableProxyContract, [], {
     deployer,
   });
-  console.log("Deployed", instance.address);
+  console.log("Deployed Proxy's Address:", proxyInstance.address);
 };
--- test/upgradeable_proxy_contract.js
+++ test/upgradeable_proxy_contract.js
@@ -1,26 +1,20 @@
-// * --- Mocha API ---
-const assert = require("assert");
+// * --- Chai API ---
+const { expect } = require("chai");
 // * --- OZ Upgrade Plugins ---
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 // * --- Contract Abstractions ---
 const UpgradeableProxyContract = artifacts.require("UpgradeableProxyContract");
 
 contract("UpgradeableProxyContract", function (accounts) {
-  let upgradeableProxyContract;
-
   beforeEach(async () => {
-    this.contract = await deployProxy(UpgradeableProxyContract);
+    this.proxyContract = await deployProxy(UpgradeableProxyContract);
   });
-  it("should grant the manager who deploys the contract the MANAGER role", async () => {
-    const managerAddress = accounts[0]; // 'deploy' or 'migrate' in truffle develop uses the first address as the account address that deploys the contract.
-    const isManager = await this.contract.hasRole(
-      await this.contract.MANAGER(),
+  it("should grant the manager who deploys the proxyContract the MANAGER role", async () => {
+    const managerAddress = accounts[0]; // 'deploy' or 'migrate' in truffle develop uses the first address as the account address that deploys the proxyContract.
+    const isManager = await this.proxyContract.hasRole(
+      await this.proxyContract.MANAGER(),
       managerAddress
     );
-    assert.equal(
-      isManager,
-      true,
-      "deploying address is not given manager role"
-    );
+    expect(isManager, "deployer address to be given Manager role").to.be.true;
   });
 });
